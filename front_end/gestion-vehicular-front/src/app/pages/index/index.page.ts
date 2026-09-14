da519097098f95e6ad2da35d145bda3b0ea8e113
Drazztyk7z <benjaoc65@gmail.com>
2026-09-14 11:20:39 -0300

    front

--- front_end/gestion-vehicular-front/src/app/pages/index/index.page.ts
+++ front_end/gestion-vehicular-front/src/app/pages/index/index.page.ts
@@ -1,19 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-index',
   templateUrl: './index.page.html',
   styleUrls: ['./index.page.scss'],
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  standalone: true,
+  imports: [
+    CommonModule, 
+    RouterLink
+  ]
 })
 export class IndexPage implements OnInit {
 
   constructor() { }
 
-  ngOnInit() {
-  }
+  ngOnInit() { }
 
 }
